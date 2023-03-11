1fb68a9a0a575391f7ad190f96b132a40aa3b104
Guntis Ozols <guntiso@uniso.lv>
2023-03-11 03:00:10 +0200

    custom function signature test

--- src/sbt-test/sbt-mojoz-simple-test-cases/table-metadata/src/main/scala/package.scala
+++ src/sbt-test/sbt-mojoz-simple-test-cases/table-metadata/src/main/scala/package.scala
@@ -5,4 +5,5 @@ import org.tresql._
 package object test {
   implicit val res = new Resources {}
   val result = tresql"foo {id, name}"
+  val test_f = tresql"foo {my_custom_db_function(name, id) x}"
 }
