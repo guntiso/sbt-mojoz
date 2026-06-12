guntiso/sbt-mojoz

commit ffadda57c90625dabc77f663c540dc7c5a9d9ffe

mojozResourceClassLoader task replacing mojozResourceLoader task removed in d39b84f6

src/main/scala/MojozPlugin.scala
@@ -12,7 +12,7 @@ import sbt.{Attributed, AutoPlugin, Compile, Def, File, config, settingKey, task
 
 object MojozPlugin extends AutoPlugin {
   object autoImport {
-
+    val mojozResourceClassLoader = taskKey[ClassLoader]("ClassLoader for resource loading for compilation")
     val mojozDtosPackage = settingKey[String]("Package where all dtos should be placed in")
     val mojozDtosImports = settingKey[Seq[String]]("List of imports for Dtos.scala")
 
@@ -70,6 +70,9 @@ object MojozPlugin extends AutoPlugin {
   import sbt.util.FileInfo
 
   override val projectSettings: Seq[Def.Setting[?]] = Seq(
+
+    mojozResourceClassLoader :=
+      MojozPlugin.getMojozResourceClassLoader((Compile / resourceDirectories).value ++ Seq((MojozMacroCompile / sbtClassDirectory).value)),
 
     mojozDtosPackage := "dto",
     mojozDtosImports := Seq(
@@ -143,7 +146,7 @@ object MojozPlugin extends AutoPlugin {
         override lazy val typeDefs            = mojozTypeDefs.value
         override lazy val tableMetadata       = mojozTableMetadata.value
         override lazy val macrosClass         = mojozTresqlMacrosClass.value.orNull
-        override lazy val resourceClassLoader = org.mojoz.MojozPlugin.getMojozResourceClassLoader((Compile / resourceDirectories).value ++ Seq((MojozMacroCompile / sbtClassDirectory).value))
+        override lazy val resourceClassLoader = mojozResourceClassLoader.value
         override lazy val uninheritableExtras = mojozUninheritableExtras.value
         override protected lazy val parserCacheSize = -1 // unlimited cache for compilation
       },

src/sbt-test/sbt-mojoz-simple-test-cases/qe-cache/build.sbt
@@ -21,7 +21,7 @@ mojozQuerease :=
     override lazy val typeDefs            = mojozTypeDefs.value
     override lazy val tableMetadata       = mojozTableMetadata.value
     override lazy val macrosClass         = mojozTresqlMacrosClass.value.orNull
-    override lazy val resourceClassLoader = org.mojoz.MojozPlugin.getMojozResourceClassLoader((Compile / resourceDirectories).value ++ Seq((Compile / classDirectory).value))
+    override lazy val resourceClassLoader = mojozResourceClassLoader.value
     override lazy val uninheritableExtras = mojozUninheritableExtras.value
     override def compileAllQueries(
       previouslyCompiledQueries: Set[String],

commit 542be6c370ea7b36006d8dce8c8e6859a0749469

mojozResourceClassLoaderFiles task replacing failing mojozResourceClassLoader from ffadda57

src/main/scala/MojozPlugin.scala
@@ -12,7 +12,7 @@ import sbt.{Attributed, AutoPlugin, Compile, Def, File, config, settingKey, task
 
 object MojozPlugin extends AutoPlugin {
   object autoImport {
-    val mojozResourceClassLoader = taskKey[ClassLoader]("ClassLoader for resource loading for compilation")
+    val mojozResourceClassLoaderFiles = taskKey[Seq[File]]("Files for classLoader for resource loading for compilation")
     val mojozDtosPackage = settingKey[String]("Package where all dtos should be placed in")
     val mojozDtosImports = settingKey[Seq[String]]("List of imports for Dtos.scala")
 
@@ -71,8 +71,8 @@ object MojozPlugin extends AutoPlugin {
 
   override val projectSettings: Seq[Def.Setting[?]] = Seq(
 
-    mojozResourceClassLoader :=
-      MojozPlugin.getMojozResourceClassLoader((Compile / resourceDirectories).value ++ Seq((MojozMacroCompile / sbtClassDirectory).value)),
+    mojozResourceClassLoaderFiles :=
+      (Compile / resourceDirectories).value ++ Seq((MojozMacroCompile / sbtClassDirectory).value),
 
     mojozDtosPackage := "dto",
     mojozDtosImports := Seq(
@@ -146,7 +146,7 @@ object MojozPlugin extends AutoPlugin {
         override lazy val typeDefs            = mojozTypeDefs.value
         override lazy val tableMetadata       = mojozTableMetadata.value
         override lazy val macrosClass         = mojozTresqlMacrosClass.value.orNull
-        override lazy val resourceClassLoader = mojozResourceClassLoader.value
+        override lazy val resourceClassLoader = MojozPlugin.getMojozResourceClassLoader(mojozResourceClassLoaderFiles.value)
         override lazy val uninheritableExtras = mojozUninheritableExtras.value
         override protected lazy val parserCacheSize = -1 // unlimited cache for compilation
       },

src/sbt-test/sbt-mojoz-simple-test-cases/qe-cache/build.sbt
@@ -16,12 +16,13 @@ mojozSchemaSqlDirectory := file("db/creation")
 
 mojozQuerease :=
   new org.mojoz.querease.Querease with org.mojoz.querease.compiling.ViewCompiler {
+    import org.mojoz.MojozPlugin
     override lazy val yamlMetadata        = mojozRawYamlMetadata.value
     override lazy val metadataConventions = mojozMdConventions.value
     override lazy val typeDefs            = mojozTypeDefs.value
     override lazy val tableMetadata       = mojozTableMetadata.value
     override lazy val macrosClass         = mojozTresqlMacrosClass.value.orNull
-    override lazy val resourceClassLoader = mojozResourceClassLoader.value
+    override lazy val resourceClassLoader = MojozPlugin.getMojozResourceClassLoader(mojozResourceClassLoaderFiles.value)
     override lazy val uninheritableExtras = mojozUninheritableExtras.value
     override def compileAllQueries(
       previouslyCompiledQueries: Set[String],